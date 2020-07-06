6e67a666eecda312d47578917a69164efe9578d2
Lulu Z <yulanda@acceptme.local>
2020-07-06 18:34:24 -0400

    well i did something

--- src/chatroom/client/user.py
+++ src/chatroom/client/user.py
@@ -1,64 +1,69 @@
-  
-# Gabriel Casciano - 06, 30, 2020
-# SimpleChatroom
-
 from tkinter import *
-from socket import socket,error
+from socket import socket, error, timeout
 from threading import *
-import struct
 
 
 class User:
-
     class Worker(Thread):
-        def __init__(self, queue: list, conn: socket, msg_var: StringVar):
+        def __init__(self, conn: socket,):
             Thread.__init__(self)
-            self.queue = queue
             self.running = True
             self.sock = conn
-            self.send_msg =""
-            self.message_variable = msg_var #message variable
+            self.send_msg = ""
+            
 
         def recv_loop(self):
-            data = "" # data var
-            self.sock.settimeout(0.5) # wait 0.5 seconds
-
+            data = ""
+            self.sock.settimeout(0.5)
             try:
                 data = self.sock.recv(2).decode()
 
-                if data ==1: # recv message
-                    data = int.from_bytes(self.sock.recv(2),"big") # recv message size
-                    data = self.sock.recv(data).decode() # recv message
-                    self.queue.append(data) # appends data to queue
+                if data == 1: # recv a msg
+                    data = int(self.sock.recv(2)) # recv msg size
+                    data = self.sock.recv(data).decode() # recv msg
+                    self.queue.append(data) # append to queue
+                    print("user recieve message")
 
-                elif data == 2: #send message
-                    data = str.encode(self.send_msg).__sizeof__() #gets message size
-                    self.sock.sendall(data) #sends message size
-                    data = str.encode(self.send_msg) #encodes message
-                    self.sock.sendall(data) #sends message
+                elif data == 2: # send a msg
+                    data = str.encode(self.send_msg).__sizeof__() #send msg size
+                    self.sock.sendall(bytes(data))
+                    data = str.encode(self.send_msg)
+                    self.sock.sendall(data)
 
-                    self.send_msg = "" # sets send_msg to nothing, whichi is important
+                    self.send_msg = ""
+                    print("user send message")
+                else:
+                    pass
 
+            except timeout as time:
+                pass
             except error as msg:
                 pass
             except Exception as exc:
+                # anything else
                 pass
 
         def msg_loop(self):
             # begin the main update loop
 
-            current_string =self.message_variable.get() #get what's on-screen
+            current_string = self.message_variable.get()
 
             for msg in self.queue:
-                current_thread += "\n" + msg # add typed out line in queue
-                lines = int.(current_string.split("\n").__len__())
-                if lines > 5: #checking if length is longer that 5 lines
-                    hold = current_string.split("\n")[1:4] # getting rid of top line to make space for new line
-                    current_string = ''.join(hold)
-
-                self.message_variable.set(current_string)
+                current_string += "\n" + msg
                 self.queue.pop()
 
+                lines = int(current_string.split("\n").__len__())
+                if lines > 5:
+                    hold = current_string.split("\n")[1:lines]
+                    current_string = ''.join(hold)
+                    
+
+                self.message_variable.set(current_string)
+
+
+        def send(self):
+            if self.send_msg != "":
+                self.sock.sendall(bytes(1)) # tell the server to prep for recv
 
         def run(self):
             while self.running:
@@ -66,88 +71,57 @@ class User:
                 self.send()
                 self.msg_loop()
 
-        def send(self):
-            if self.send_msg != "": #if msg isn't nothing
-                self.sock.sendall(struct.pack("B", 1)) # tells server to prepare to recv
-
-        def add_to_queue(self,msg): #called by gui
+        def add_to_queue(self, msg):  # also called by gui
             self.send_msg = msg
             pass
 
-        def start(self):
-            pass
-    
     def __init__(self, tk: Tk, conn: socket):
         self.queue = []
         self.sock = conn  # no need to connect this socket object because it already was in login
         self.username = ""
         self.message_variable = StringVar()
-        self.worker = self.Worker(self.queue, self.sock, self.message_variable)
-        self.worker.start() #starts worker
+        self.worker = self.Worker(self.sock)
+        self.worker.start()
 
-        self.tk = tk #makes window
+        self.tk = tk
         tk.title("Chatroom - Main Window")
-        tk.geometry("380x160")
-        tk.resizable(0,0) #can't change window size
+        tk.geometry("350x160")
+        tk.resizable(0,0)
 
-        self.left_frame = Frame(tk,width =300) #makes frame 
-        self.right_frame = Frame(tk,width = 100)
+        self.left_frame = Frame(tk)
+        self.right_frame = Frame(tk)
 
-        # packs in left frame
-        self.message_label = Label(self.left_frame, textvariable=self.message_variable, wraplength=250, justify=LEFT,anchor=NW) #ancor starts on northwest.
+        self.message_label = Label(self.left_frame, textvariable=self.message_variable, wraplength=250, justify=LEFT, anchor=NW)
         self.message_entry = Entry(self.left_frame)
 
-        #  packs buttons into right frame
         self.enter_button = Button(self.right_frame, text="Enter", command=self.enter_button_command)
-        self.clear_button = Button(self.right_frame, text="Clear", command =self.clear_button_command)
-        self.logout_button = Button(self.right_frame, text="Log Out")
+        self.clear_button = Button(self.right_frame, text="Clear", command=self.clear_button_command)
+        self.logout_button = Button(self.right_frame, text="Log Out", command=self.logout_button_command)
 
-        self.message_variable.set("Hello my name is gabriel casciano \n this is a test")
+        self.message_variable.set("This is an empty chat window")
 
-        #pack left frame
+        # Pack the left frame
         self.message_label.pack(side=TOP, fill=X)
         self.message_entry.pack(side=BOTTOM, fill=X)
-        self.left_frame.place(x=5,y=5,width=275,height=150)
-        # pack right frame
-        self.enter_button.pack(side=BOTTOM,fill=X)
+        self.left_frame.place(x=5, y=5, width=275, height=150)
+        # Pack the right frame
+        self.enter_button.pack(side=BOTTOM, fill=X)
         self.clear_button.pack(side=BOTTOM, fill=X)
-        self.logout_button.pack(side=BOTTOM,fill=X)
-        self.right_frame.place(x=280,y=5,width=80,height=150)
+        self.logout_button.pack(side=BOTTOM, fill=X)
+        self.right_frame.place(x=285, y=5, width=60, height=150)
 
-    def enter_button_command(self): # click enter button
-        self.worker.add_to_queue(self.username + ">" + self.message_entry.get()) #adds text to queue
-        self.message_entry.delete(0,"end") # clears text box
+    def enter_button_command(self):
+        self.worker.add_to_queue(self.username + ">" + self.message_entry.get())
+        self.message_entry.delete(0, "end")
 
-    def clear_button_command(self): #click clear button
-        #clears the thing
-        self.message_entry.delete(0,"end")
+    def clear_button_command(self):
+        self.message_entry.delete(0, "end")
         self.message_variable.set("")
 
-
-    def logout_button(self):
+    def logout_button_command(self):
         pass
 
     def identify(self):
-        self.sock.sendall(3) # asks server who we are
-        self.username = self.sock.recv(4).decode() #gets username
+        self.sock.sendall(3)  # ask the server who we are
+        self.username = self.sock.recv(4).decode()
 
-
-    def main_loop(self):
-        #  begin main update loop
-        self.message_variable.set("")
-
-        for msg in self.queue:
-            self.message_variable.set(self.message_variable.get() + "\n"+ msg)
-
-        if self.queue.__len__() > self.length_of_queue: #makes sure only 10 messages show
-            self.queue.pop() # pops off top (oldest) message
-
-        pass
-
-if __name__ == '__main__':
-    root = Tk()
-    gui = User(root, socket())
-
-    gui.identify()
-    root.mainloop()
-    gui.worker.join()
--- src/chatroom/server/server.py
+++ src/chatroom/server/server.py
@@ -1,7 +1,7 @@
+import struct
 from socket import *
 from threading import *
 import csv
-import struct #converting from int to byte using struct.pack("B", thing)
 
 HOST = ""
 PORT = 12345
@@ -13,118 +13,125 @@ clients = []
 
 class Session(Thread):
 
-    def __init__(self, conn:socket):
+    def __init__(self, conn: socket):
         Thread.__init__(self)
-        self.data =""
+        self.data = ""
         self.command = 0
         self.sock = conn
         self.recv_flag = False
         self.running = True
         self.username = ""
 
-
-
     def recv_loop(self):
-        self.sock.settimeout(0.5) #wait time less, 
+        self.sock.settimeout(0.5)
         try:
-            if self.command == 1: #send data command
+            if self.command == 1:  # send data command
                 # only issues by the server
-                data = str.encode(self,data)
-                self.sock.sendall((struc.pack("B",self.command))
-                self.sock.sendall(struc.pack("B",data.__len__()))
+                data = str.encode(self.data)
+                self.sock.sendall(struct.pack("B", self.command))
+                self.sock.sendall(struct.pack("B", data.__len__()))
                 self.sock.sendall(data)
                 self.command = 0
+                print(" server sent message, command 1")
 
-            elif self.command == 2: #recv data command
+            elif self.command == 2:  # recv data command
                 # only issued by the client to the host
-                self.sock.sendall(struc.pack("B",self.command))
-                data = int.from_bytes(self.sock.recv(2),"big")
-                self.data = self.rock.recv(data).decode()
-                self.recv_flag = True
+                self.sock.sendall(struct.pack("B", self.command))
+                data = int.from_bytes(self.sock.recv(2) , "big")
+                self.data = self.sock.recv(data).decode()
                 
+                self.recv_flag = True
+                print(" server recieve message, command 2")
                 self.command = 0
 
             elif self.command == 3:
                 if self.username is not "":
                     self.sock.sendall(str.encode(self.username))
+                    print("sends username")
 
                 self.command = 0
+                print("command 3 was done")
 
-            if self.command == 0:# wait for user ot issue a command
-                self.command = int.from_bytes(self.sock.recv(2),"big")
+            if self.command == 0:  # wait for user to issue a command
+                self.command = int.from_bytes(self.sock.recv(2), "big")
+                print("server reiceved command")
 
         except timeout as timeExcp:
+            print("timeout error?")
             pass
         except error as errMsg:
+            print("error error?")
             pass
+
         except Exception as exc:
+            print("green excepion error thing")
             # anything else
             pass
 
+    def login(self):
+        creds = []
+        with open('notPasswords.csv', "r", newline='') as file:
+            reader = csv.reader(file, delimiter=',')
+
+            for row in reader:
+                creds.append(row)
+
+        data = self.sock.recv(256).decode().split(',')
+
+        if data in creds:
+            self.sock.sendall(struct.pack("B", 1))
+            self.username = data[0]
+            print("correct password")
+            return True
+        else:
+            self.sock.sendall(struct.pack("B", 2))
+            print("wrong password/something else")
+            return False
+
+    def run(self):
+        if self.login():
+            while self.running:
+                self.recv_loop()
+                print("doing the recieve loop")
 
 
-def login(self): # check username/password
-    creds = []
-    with open('maybePasswords.csv', "r", newline=' ') as file:
-        reader = reader.csv(file, delimiter=')
-
-        for row in reader:
-            creds.append(row)
-
-    data = self.sock.recv(256).decode().spilt(',')
-
-    if data in creds: # if right
-        self.sock.sendall(struct.pack("B",1)) #right passowrds
-        self.username = data[0] 
-        return True
-    else: #if wrong
-        self.sock.sendall(struct.pack("B",3)) #incrorect thing
-        return False
-
-
-
-def run(self):
-    if self.login():
-        while self.running:
-            self.recv_loop()
-
-
-
-class Router(Thread): #router thread
-    def __init__(self,conn:socket):
+class Router(Thread):
+    def __init__(self, conn: socket):
+        Thread.__init__(self)
         self.sock = conn
         self.sock.listen()
         self.running = True
 
     def run(self):
         while self.running:
-            conn,addr = self.sock.accept()
-            session = Session(conn) #session is mirror version of user thats on server
-            if session not in clients: #if session not in client list, add it
+            conn, addr = self.sock.accept()
+            session = Session(conn)
+            if session not in clients: #session is mirror version of user that's on server
+                #if the session doesn't exist, add it on to clients list
                 session.start()
-                clients.append(session)
-
-
+                clients.append(session) 
+                print("added new session")
+                
 
 
 def runtime():
-    data=[]
+    data = []
     for c in clients:
-        if c.recv_flag: # someone sent data
-            data.append(c.data) #copy and break
-            c.recv_flag = False # set flag to false
+        if c.recv_flag:  # someone has sent data
+            data.append(c.data)  # copy and break
+            c.recv_flag = False # reset the flag
+            print("data was sent and flag has been reset")
 
     for d in data:
         if d is not "":
-            for c in clients: #send data to all
+            for c in clients:  # send data to all
                 c.command = 1
                 c.data = data
-
-
+                print("doing the sending thing")
 
 
 if __name__ == '__main__':
-    main_sock.bind((HOST,PORT))
+    main_sock.bind((HOST, PORT))
     router = Router(main_sock)
     router.start()
 
