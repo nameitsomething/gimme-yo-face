d627d81378b7f92ff6c14ba736eb1a90f5b3e939
Lulu Z <yulanda@acceptme.local>
2020-07-06 20:04:35 -0400

    chicken wants food

--- src/chatroom/src/client/user.py
+++ src/chatroom/src/client/user.py
@@ -1,3 +1,4 @@
+import struct
 from tkinter import *
 from socket import socket, error, timeout
 from threading import *
@@ -5,33 +6,31 @@ from threading import *
 
 class User:
     class Worker(Thread):
-        def __init__(self, conn: socket,):
+        def __init__(self, conn: socket):
             Thread.__init__(self)
+            self.queue = []
             self.running = True
             self.sock = conn
             self.send_msg = ""
-            
 
         def recv_loop(self):
             data = ""
             self.sock.settimeout(0.5)
             try:
-                data = self.sock.recv(2).decode()
+                data = int.from_bytes(self.sock.recv(2), "big")
 
                 if data == 1: # recv a msg
-                    data = int(self.sock.recv(2)) # recv msg size
+                    data = int.from_bytes(self.sock.recv(2), "big") # recv msg size
                     data = self.sock.recv(data).decode() # recv msg
                     self.queue.append(data) # append to queue
-                    print("user recieve message")
 
                 elif data == 2: # send a msg
-                    data = str.encode(self.send_msg).__sizeof__() #send msg size
-                    self.sock.sendall(bytes(data))
+                    data = str.encode(self.send_msg).__sizeof__()
+                    self.sock.sendall(struct.pack("B", data))
                     data = str.encode(self.send_msg)
                     self.sock.sendall(data)
 
                     self.send_msg = ""
-                    print("user send message")
                 else:
                     pass
 
@@ -43,37 +42,20 @@ class User:
                 # anything else
                 pass
 
-        def msg_loop(self):
-            # begin the main update loop
-
-            current_string = self.message_variable.get()
-
-            for msg in self.queue:
-                current_string += "\n" + msg
-                self.queue.pop()
-
-                lines = int(current_string.split("\n").__len__())
-                if lines > 5:
-                    hold = current_string.split("\n")[1:lines]
-                    current_string = ''.join(hold)
-                    
-
-                self.message_variable.set(current_string)
-
-
         def send(self):
             if self.send_msg != "":
-                self.sock.sendall(bytes(1)) # tell the server to prep for recv
+                self.sock.sendall(struct.pack("B", 2))  # tell the server to prep for recv
 
         def run(self):
             while self.running:
                 self.recv_loop()
                 self.send()
-                self.msg_loop()
 
         def add_to_queue(self, msg):  # also called by gui
             self.send_msg = msg
-            pass
+
+        def get_queue(self):
+            return self.queue
 
     def __init__(self, tk: Tk, conn: socket):
         self.queue = []
@@ -82,6 +64,8 @@ class User:
         self.message_variable = StringVar()
         self.worker = self.Worker(self.sock)
         self.worker.start()
+
+        self.flag = 0
 
         self.tk = tk
         tk.title("Chatroom - Main Window")
@@ -112,6 +96,7 @@ class User:
 
     def enter_button_command(self):
         self.worker.add_to_queue(self.username + ">" + self.message_entry.get())
+        self.worker.send()
         self.message_entry.delete(0, "end")
 
     def clear_button_command(self):
@@ -122,6 +107,25 @@ class User:
         pass
 
     def identify(self):
-        self.sock.sendall(3)  # ask the server who we are
+        self.sock.sendall(struct.pack("B", 3))  # ask the server who we are
         self.username = self.sock.recv(4).decode()
 
+    def get_flag(self):
+        return self.flag
+
+    def msg_loop(self):
+        # begin the main update loop
+        current_string = self.message_variable.get()
+
+        self.queue = self.worker.get_queue()
+
+        for msg in self.queue:
+            current_string += "\n" + msg
+            self.queue.pop()
+
+            lines = int(current_string.split("\n").__len__())
+            if lines > 5:
+                hold = current_string.split("\n")[1:lines]
+                current_string = ''.join(hold)
+
+            self.message_variable.set(current_string)
